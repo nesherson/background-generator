1732d2e08d9233240ada2699b62ebc6c1273f058
unknown <nesad.omeragich27@gmail.com>
2020-04-22 14:12:56 +0200

    added angle property

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
-var currentBackground = document.querySelector("h3");
-var color_one = document.querySelector(".input-color-one");
-var color_two = document.querySelector(".input-color-two");
-var body = document.querySelector("body");
+const currentBackground = document.querySelector('h3');
+const color_one = document.querySelector('.input-color-one');
+const color_two = document.querySelector('.input-color-two');
+const body = document.querySelector('body');
+const range = document.querySelector('.input-range');
 
 function setGradient() {
   body.style.background =
-    "linear-gradient(to right, " +
-    color_one.value +
-    ", " +
-    color_two.value +
-    ")";
-
-  currentBackground.textContent = body.style.background + ";";
+    'linear-gradient(90deg, ' + color_one.value + ', ' + color_two.value + ')';
+  currentBackground.textContent = body.style.background + ';';
 }
 
-color_one.addEventListener("input", setGradient);
+function setDegree() {
+  body.style.background =
+    'linear-gradient(' +
+    range.value +
+    'deg,' +
+    color_one.value +
+    ', ' +
+    color_two.value +
+    ')';
+  currentBackground.textContent = body.style.background + ';';
+}
 
-color_two.addEventListener("input", setGradient);
+color_one.addEventListener('input', setGradient);
+
+color_two.addEventListener('input', setGradient);
+
+range.addEventListener('input', setDegree);
